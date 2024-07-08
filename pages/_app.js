0c488f3f3311bc44fa3ff5d2c9edb4a8061175d0
santiago <frontend@arcoch.com>
2024-07-08 15:02:23 -0300

    change directory for image

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import '@/styles/globals.css'
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>To-Do List Santiago Cervan</title>
+        <meta name="description" content="Una aplicación de lista de tareas construida con React y Next.js" />
+        <meta property="og:title" content="To-Do List Santiago Cervan" />
+        <meta property="og:description" content="Una aplicación de lista de tareas construida con React y Next.js" />
+        <meta property="og:image" content="/og-image.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
